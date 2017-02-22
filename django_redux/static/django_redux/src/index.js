688f05a0da1b61a38001c749263a933c794305c4
Flavio Curella <flavio.curella@gmail.com>
2017-02-22 10:38:03 -0600

    remove transform functions. added docs

--- django_redux/static/django_redux/src/index.js
+++ django_redux/static/django_redux/src/index.js
@@ -1,17 +1,37 @@
 import ReconnectingWebSocket from 'reconnecting-websocket';
 
 
-const identity = (action, stream) => {
-  return action;
-}
+const noop = (...args) => {};
 
-
+/**
+ * Bridge between Channels and plain javascript.
+ *
+ * @example
+ * const webSocketBridge = new WebSocketBridge();
+ * webSocketBridge.connect();
+ * webSocketBridge.listen(function(action, stream) {
+ *   console.log(action, stream);
+ * });
+ */
 export class WebSocketBridge {
-  constructor() {
+  constructor(options) {
     this._socket = null;
     this.streams = {};
+    this.default_cb = null;
+    this.options = Objec.assign({}, {
+      onopen: noop,
+    }, options);
   }
 
+  /**
+   * Connect to the websocket server
+   *
+   * @param      {string}  [url]     The url of the websocket. Defaults to
+   * `window.location.host`
+   * @example
+   * const webSocketBridge = new WebSocketBridge();
+   * webSocketBridge.connect();
+   */
   connect(url) {
     let _url;
     if (url === undefined) {
@@ -24,17 +44,67 @@ export class WebSocketBridge {
     this._socket = new ReconnectingWebSocket(_url);
   }
 
-  reconnect(state) {
-    // add recovery logic here..
+  /**
+   * Starts listening for messages on the websocket, demultiplexing if necessary.
+   *
+   * @param      {Function}  [cb]         Callback to be execute when a message
+   * arrives. The callback will receive `action` and `stream` parameters
+   *
+   * @example
+   * const webSocketBridge = new WebSocketBridge();
+   * webSocketBridge.connect();
+   * webSocketBridge.listen(function(action, stream) {
+   *   console.log(action, stream);
+   * });
+   */
+  listen(cb) {
+    this.default_cb = cb;
+    this._socket.onmessage = (event) => {
+      const msg = JSON.parse(event.data);
+      let action;
+      let stream;
+      if (msg.stream !== undefined && this.streams[msg.stream] !== undefined) {
+        action = msg.payload;
+        stream = msg.stream;
+        const stream_cb = this.streams[stream];
+        stream_cb ? stream_cb(action, stream) : null;
+      } else {
+        action = msg;
+        stream = null;
+        this.default_cb ? this.default_cb(action, stream) : null;
+      }
+    };
+
+    this._socket.onopen = this.options.onopen;
+  }
+
+  /**
+   * Adds a 'stream handler' callback. Messages coming from the specified stream
+   * will call the specified callback.
+   *
+   * @param      {string}    stream  The stream name
+   * @param      {Function}  cb      Callback to be execute when a message
+   * arrives. The callback will receive `action` and `stream` parameters.
+
+   * @example
+   * const webSocketBridge = new WebSocketBridge();
+   * webSocketBridge.connect();
+   * webSocketBridge.listen();
+   * webSocketBridge.demultiplex('mystream', function(action, stream) {
+   *   console.log(action, stream);
+   * });
+   * webSocketBridge.demultiplex('myotherstream', function(action, stream) {
+   *   console.info(action, stream);
+   * });
+   */
+  demultiplex(stream, cb) {
+    this.streams[stream] = cb;
   }
 
   send(action) {
     this._socket.send(JSON.stringify(action));
   }
 
-  demultiplex(stream, transform = identity) {
-    this.streams[stream] = transform;
-  }
   stream(stream) {
     return {
       send: (action) => {
@@ -49,9 +119,37 @@ export class WebSocketBridge {
 
 }
 
+/**
+ * Convenience singleton for `WebSocketBridge`.
+ * @example
+ * import { webSocketBridge } from 'django_redux';
+ *
+ * webSocketBridge.connect();
+ * webSocketBridge.listen(function(action, stream) { console.log(action) });
+ *
+ * @type       {WebSocketBridge}
+ */
+export const webSocketBridge = new WebSocketBridge()
 
+/**
+ * Bridge between Channels and Redux.
+ * By default dispatches actions received from channels to the redux store.
+ *
+ * @example
+ * const reduxBridge = new ReduxBridge();
+ * reduxBridge.connect();
+ * reduxBridge.listen(store);
+ */
 class ReduxBridge extends WebSocketBridge {
-  listen(store, transform = identity) {
+  constructor(options) {
+    options = Object.assign({}, {
+      onreconnect: noop,
+    }, options);
+    super(options);
+  }
+
+  listen(store, cb = this.storeDispatch) {
+    this.default_cb = cb;
     this._socket.onmessage = (event) => {
       const msg = JSON.parse(event.data);
       let action;
@@ -59,12 +157,13 @@ class ReduxBridge extends WebSocketBridge {
       if (msg.stream !== undefined && this.streams[msg.stream] !== undefined) {
         action = msg.payload;
         stream = msg.stream;
-        transform = this.streams[stream];
+        const stream_cb = this.streams[stream];
+        stream_cb(store, action, stream)
       } else {
         action = msg;
         stream = null;
+        this.default_cb(store, action, stream);
       }
-      this.receiveSocketMessage(store, action, stream, transform);
     };
 
     this._socket.onopen = () => {
@@ -72,23 +171,26 @@ class ReduxBridge extends WebSocketBridge {
 
       if (state.currentUser !== null) {
         // the connection was dropped. Call the recovery logic
-        this.reconnect(state);
+        this.options.onreconnect(state);
       }
     };
 
   }
 
-  receiveSocketMessage(store, action, stream, transform) {
-    /* We cheat by using the Redux-style Actions as our
-     * communication protocol with the server. This hack allows
-     * the server to directly act as a Action Creator, which we
-     * simply `dispatch()`.  Consider separating communication format
-     * from client-side msg API.
-     */
-    return store.dispatch(transform(action));
+  storeDispatch(store, action, stream) {
+    return store.dispatch(action);
   }
 
 }
 
+/**
+ * Convenience singleton for `ReduxSocketBridge`.
+ * @example
+ * import { ReduxBridge } from 'django_redux';
+ *
+ * ReduxBridge.connect();
+ * ReduxBridge.listen(store);
+ *
+ * @type       {ReduxSocketBridge}
+ */
 export const reduxBridge = new ReduxBridge()
-export default reduxBridge;
