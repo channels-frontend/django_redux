6b04ba5ec229cef5b0fa405e69d5027e4eff7eed
Flavio Curella <flavio.curella@gmail.com>
2017-04-03 22:42:35 +0200

    recompile js

--- django_redux/static/django_redux/js/websocketbridge.js
+++ django_redux/static/django_redux/js/websocketbridge.js
@@ -423,8 +423,6 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.reduxBridge = exports.ReduxBridge = undefined;
 
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
-
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _djangoChannels = require('django-channels');
@@ -435,44 +433,38 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var noop = function noop() {};
-
 var ReduxBridge = exports.ReduxBridge = function (_WebSocketBridge) {
   _inherits(ReduxBridge, _WebSocketBridge);
 
-  function ReduxBridge(options) {
+  function ReduxBridge() {
     _classCallCheck(this, ReduxBridge);
 
-    options = _extends({}, {
-      onreconnect: noop
-    }, options);
-    return _possibleConstructorReturn(this, (ReduxBridge.__proto__ || Object.getPrototypeOf(ReduxBridge)).call(this, options));
+    return _possibleConstructorReturn(this, (ReduxBridge.__proto__ || Object.getPrototypeOf(ReduxBridge)).apply(this, arguments));
   }
-
-  /**
-   * Starts listening for messages on the websocket, demultiplexing if necessary.
-   *
-   * @param      {Object}  store         Your redux store.
-   * @param      {Function}  [cb]         Callback to be execute when a message
-   * arrives. The callback will receive `store`, `action` and `stream` parameters.
-   * By default it will call `store.dispatch(action)`;
-   *
-   * @example
-   * const webSocketBridge = new WebSocketBridge();
-   * webSocketBridge.connect();
-   * webSocketBridge.listen(store);
-   */
-
 
   _createClass(ReduxBridge, [{
     key: 'listen',
+
+    /**
+     * Starts listening for messages on the websocket, demultiplexing if necessary.
+     *
+     * @param      {Object}  store         Your redux store.
+     * @param      {Function}  [cb]         Callback to be execute when a message
+     * arrives. The callback will receive `store`, `action` and `stream` parameters.
+     * By default it will call `store.dispatch(action)`;
+     *
+     * @example
+     * const webSocketBridge = new WebSocketBridge();
+     * webSocketBridge.connect();
+     * webSocketBridge.listen(store);
+     */
     value: function listen(store) {
       var _this2 = this;
 
       var cb = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : this.storeDispatch;
 
       this.default_cb = cb;
-      this._socket.onmessage = function (event) {
+      this.socket.onmessage = function (event) {
         var msg = JSON.parse(event.data);
         var action = void 0;
         var stream = void 0;
@@ -488,7 +480,7 @@ var ReduxBridge = exports.ReduxBridge = function (_WebSocketBridge) {
         }
       };
 
-      this._socket.onopen = function () {
+      this.socket.onopen = function () {
         var state = store.getState();
 
         if (state.currentUser !== null) {
